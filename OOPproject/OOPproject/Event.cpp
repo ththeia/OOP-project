809f22b8f8b6c5679a238172814e46ff43f64da1
Theodora Cupas <theodora.cupas@gmail.com>
2023-11-05 21:35:49 +0200

    Int main Events try

--- OOPproject/OOPproject/Event.cpp
+++ OOPproject/OOPproject/Event.cpp
@@ -12,31 +12,47 @@ private:
     int* seatNumbers;
 
 public:
-    Event()
-        : maxSeats(0), seatNumbers(nullptr) {}
-
-    Event(const string& name, const string& date, const string& time, int maxSeats)
-        : name(name), date(date), time(time), maxSeats(maxSeats) {
-        this->seatNumbers = new int[maxSeats];
+    Event() {
+        name = "";
+        date = "";
+        time = "";
+        maxSeats = 0;
+        seatNumbers = nullptr;
     }
+
+    Event(const string& name, const string& date, const string& time, int maxSeats) {
+        this->name = name;
+        this->date = date;
+        this->time = time;
+        this->maxSeats = maxSeats;
+
+        if (maxSeats > 0) {
+            this->seatNumbers = new int[maxSeats];
+        }
+        else {
+            this->seatNumbers = nullptr;
+        }
+    }
+
 
     ~Event() {
-        delete[] this->seatNumbers;
+        if (this->seatNumbers != nullptr) {
+            delete[] this->seatNumbers;
+        }
     }
-
-    const string& getName() const {
+    const string& getName()  {
         return this->name;
     }
 
-    const string& getDate() const {
+    const string& getDate()  {
         return this->date;
     }
 
-    const string& getTime() const {
+    const string& getTime() {
         return this->time;
     }
 
-    int getMaxSeats() const {
+    int getMaxSeats()  {
         return this->maxSeats;
     }
 
@@ -73,3 +89,22 @@ public:
         return out;
     }
 };
+
+
+    int main() {
+
+        Event event1;    
+        cin >> event1;
+
+        
+        if (event1.isValid()) {
+            cout << "Event Details:\n" << event1;
+        }
+        else {
+            cout << "Invalid event details entered.\n";
+        }
+        Event event2("Event 2", "2023-11-05", "15:00", 100);
+        cout << "Event Details:\n" << event2;
+
+
+    };
